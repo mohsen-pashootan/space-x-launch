380dbb74c81bd250b0408f12a6006b50c7e73b17
unknown <tahmasebpour.mohsen@gmail.com>
2020-09-20 14:00:32 +0430

    make components for get data and showing

---
+++ src/component/pastLaunches.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function PastLaunches() {
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>Flight Number </th>
+            <th>Mission Name</th>
+            <th>Launch Date</th>
+            <th>Launch Site</th>
+            <th>Details</th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
+    </>
+  );
+}
---
+++ src/component/upcomingLaunches.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function UpcomingLaunches() {
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>Flight Number </th>
+            <th>Mission Name</th>
+            <th>Launch Date</th>
+            <th>Launch Site</th>
+            <th>Details</th>
+          </tr>
+        </thead>
+      </table>
+    </>
+  );
+}
