4775448a53a7bbe8786e6e09b4d809c7bc83f6a4
unknown <tahmasebpour.mohsen@gmail.com>
2020-09-20 13:59:48 +0430

    make components for get data and showing past and upcoming  launches

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
-import "./App.css";
+import PastLaunches from "./component/pastLaunches";
+import UpcomingLaunches from "./component/upcomingLaunches";
 import Layout from "./sharedComponent/layout";
 
 function App() {
   const [mode, setMode] = useState("earth");
+
+  function handleOnPast() {
+    setMode("past");
+  }
+  function handleOnLaunch() {
+    setMode("launch");
+  }
   return (
-    <Layout onPast onLuanch>
-      {}
+    <Layout onPast={handleOnPast} onLuanch={handleOnLaunch}>
+      {mode === "past" && <PastLaunches />}
+      {mode === "launch" && <UpcomingLaunches />}
     </Layout>
   );
 }
